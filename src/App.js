dd8875c4f8ca0c9897063a35b523759a198ec5d5
Rich Hildred <rhildred@ip-172-31-5-173.us-west-2.compute.internal>
2018-10-02 14:38:52 -0400

    made app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      sQuestion:"",
+      sAnswer:"",
+      sQuestionInput:""
+    };
+    this.aAnswers=[
+      "yes", "no", "maybe", "perhaps", "definitely", "never", "most likely"
+    ]
+  }
+  askQuestion(){
+    this.setState({sQuestion: this.state.sQuestionInput});
+    let nRandom = Math.floor(Math.random()*this.aAnswers.length);
+    this.setState({sAnswer: this.aAnswers[nRandom]});
+    this.setState({sQuestionInput: ""});
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up src/App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Welcome to the magic 8 ball!</Text>
+        <Text>Type a question with a yes or no answer in the box below.</Text>
+        <Text>Prepare to be amazed.</Text>
+        <TextInput 
+                ref='questionInput'
+                placeholder="Type your question here!"
+                value={this.state.sQuestionInput}
+                onChangeText={(text) => this.setState({ sQuestionInput: text })}
+                onSubmitEditing={() => this.askQuestion()}
+                autoFocus={true}
+                blurOnSubmit={false}
+        />
+        <Text>{this.state.sQuestion}</Text>
+        <Text>{this.state.sAnswer}</Text>
       </View>
     );
   }
